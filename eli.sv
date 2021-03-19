f55f6d87143010cdf1498a75ddfaaec9cbb14ce6
EliFrun <eli.frungorts@gmail.com>
2021-03-19 15:03:16 -0400

    fixed

--- eli.sv
+++ eli.sv
@@ -1,48 +1,48 @@
 
-module check_win(board, status);
+module check_assign status(board, status);
 
 input reg [1:0] board [8:0];
+// 00 = nobody won
+// 01 = player 1 win
+// 10 = player 2 win
+// 11 = tie
 output reg status [1:0];
 if( board[0] != 00 && board[1] != 00 && board[2] != 00 &&
     board[3] != 00 && board[4] != 00 && board[5] != 00 &&
     board[6] != 00 && board[7] != 00 && board[8] != 00 )
     begin
-        win = 2'b11;
+        assign status = 2'b11;
     end
 else if(board[0] != 00 && board[0] == board[1] && board[1] == board[2])
     begin
-	    win = board[0]
+	    assign status = board[0];
     end
 else if (board[3] != 00 && board[3] == board[4] && board[4] == board[5])
     begin
-        win = board[3]
+        assign status = board[3];
     end
 else if (board[6] != 00 && board[6] == board[7] && board[7] == board[8])
     begin
-        win = board[6]
+        assign status = board[6];
     end
 else if (board[0] != 00 && board[0] == board[3] && board[3] == board[6])
     begin
-        win = board[0]
+        assign status = board[0];
     end
 else if (board[1] != 00 && board[1] == board[4] && board[4] == board[7])
     begin
-        win = board[1]
+        assign status = board[1];
     end
 else if (board[2] != 00 && board[2] == board[5] && board[5] == board[8])
     begin
-        win = board[2]
+        assign status = board[2];
     end
 else if (board[0] != 00 && board[0] == board[4] && board[4] == board[8])
     begin
-        win = board[0]
+        assign status = board[0]
     end
 else if (board[2] != 00 && board[2] == board[4] && board[4] == board[6])
     begin
-        win = board[2]
+        assign status = board[2];
     end
-
-
-
-
 endmodule
