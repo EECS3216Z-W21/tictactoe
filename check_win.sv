93b723ba1b50c48ecb824819f0d4e37973ca5a99
EliFrun <eli.frungorts@gmail.com>
2021-03-19 17:48:18 -0400

    foo

--- check_win.sv
+++ check_win.sv
@@ -1,50 +1,47 @@
 
-module check_win(board, status);
+function [1:0] check_win(input [1:0] board [8:0]);
 
-input reg [1:0] board [8:0];
 // 00 = nobody won
 // 01 = player 1 win
 // 10 = player 2 win
 // 11 = tie
-output reg [1:0] status;
 
-always
 if( board[0] !== 00 && board[1] !== 00 && board[2] !== 00 &&
     board[3] !== 00 && board[4] !== 00 && board[5] !== 00 &&
     board[6] !== 00 && board[7] !== 00 && board[8] !== 00 )
     begin
-         status <= 2'b11;
+         check_win = 2'b11;
     end
 else if(board[0] !== 00 && board[0] == board[1] && board[1] == board[2])
     begin
-	     status <= board[0];
+	     check_win = board[0];
     end
 else if (board[3] !== 00 && board[3] == board[4] && board[4] == board[5])
     begin
-         status <= board[3];
+         check_win = board[3];
     end
 else if (board[6] !== 00 && board[6] == board[7] && board[7] == board[8])
     begin
-         status <= board[6];
+         check_win = board[6];
     end
 else if (board[0] !== 00 && board[0] == board[3] && board[3] == board[6])
     begin
-         status <= board[0];
+         check_win = board[0];
     end
 else if (board[1] !== 00 && board[1] == board[4] && board[4] == board[7])
     begin
-         status <= board[1];
+         check_win = board[1];
     end
 else if (board[2] !== 00 && board[2] == board[5] && board[5] == board[8])
     begin
-         status <= board[2];
+         check_win = board[2];
     end
 else if (board[0] !== 00 && board[0] == board[4] && board[4] == board[8])
     begin
-         status <= board[0];
+         check_win = board[0];
     end
 else if (board[2] !== 00 && board[2] == board[4] && board[4] == board[6])
     begin
-         status <= board[2];
+         check_win = board[2];
     end
-endmodule
+endfunction
--- main.sv
+++ main.sv
@@ -1,122 +1,155 @@
 //Part 1: Module header:
-module main(clk, rst, move);
-// , output logic z
+module main(clk, select, rst, move);
+	// , output logic z
+	
+	input clk, select, rst;
+	input reg [8:0]move;
 
-input logic clk;
-input logic rst;
-input reg [8:0]move;
+	//Part 2: Declarations:
 
-//Part 2: Declarations:
+	//FSM-related declarations:
+	typedef enum logic [2:0] {play, checkMove, checkWin, p1Win, p2Win, tie, reset} state;
+	
+	state pr_state, nx_state;
 
-//FSM-related declarations:
-typedef enum logic [2:0] {play, checkWin, p1Win, p2Win, tie, reset} state;
-state pr_state, nx_state;
-
-//Modules used
-//check_input(location[8:0], board, isValid);
-
-/* Check_win
-00 => no win
-01 => p1
-10 => p2
-11 => tie/cat's game
-*/
-
-check_win(board, winner);
-
-//this is a board made of 9 2-bit spaces 
-//0 1 2
-//3 4 5
-//6 7 8
-
-//00 => unoccupied
-//01 => unused
-//10 => player 1
-//11 => player 2
-reg [1:0] board [8:0]; 
+	// functions used
+	// check_input(location[8:0], board, isValid);
 
 
-reg player; //this is the current player
-//0 => player 1
-//1 => player 2
+	//this is a board made of 9 2-bit spaces 
+	//0 1 2
+	//3 4 5
+	//6 7 8
 
-reg [8:0] location; //this is the location a player is making a move to
+	//00 => unoccupied
+	//01 => player 1
+	//10 => player 2
+	//11 => unused
+	reg [1:0] board [8:0] = '{
+		2'b00,2'b00,2'b00,
+		2'b00,2'b00,2'b00,
+		2'b00,2'b00,2'b00}; 
 
-wire isValid; //is a move valid
 
-wire [1:0] winner; //output of checkWin
+	reg player; //this is the current player
+	//0 => player 1
+	//1 => player 2
 
-//TODO - RESET BUTTON TIMER LOGIC
-//Timer-related declarations:
-//const logic [7:0] T1 = <value>, tmax = <value>;
-//always_ff @(posedge clk, posedge rst) if (rst) t <= 0;
-//else if (t < tmax) t <= t + 1; else t <= 0;
+	reg [8:0] location; //this is the location a player is making a move to
+	wire [1:0] winner; //output of checkWin
 
-//FSM state register:
-always_ff @(posedge clk, posedge rst)
- if(rst) pr_state <= play; else pr_state <= nx_state;
+	//TODO - RESET BUTTON TIMER LOGIC
+	//Timer-related declarations:
+	//const logic [7:0] T1 = <value>, tmax = <value>;
+	//always_ff @(posedge clk, posedge rst) if (rst) t <= 0;
+	//else if (t < tmax) t <= t + 1; else t <= 0;
 
-//FSM combinational logic: 
-always
-case(pr_state)
-	 play: begin
-		//user input
-		//TODO
+
+	parameter frequency = 10000000;
+	
+	reg [31:0] counter = 32'd0;
+	parameter fps = 60;
+	parameter clocks_per_frame = frequency/fps;
+	
+	// 1000 hz rst polling
+	reg [31:0] button_counter = 32'd0;
+	parameter polling_rate = frequency/1000;
+	always @(posedge clk) begin
+		button_counter <= button_counter + 1;
+		// negedge triggering the rst doesn't work too well
+		// better to simply poll every ms and let the state transition
+		// at the next frame
+		if(button_counter >= polling_rate) begin
+			if(!rst) begin
+				button_counter <= 0;
+				nx_state <= play;
+				player <= 0;
+				board <= '{
+					2'b00,2'b00,2'b00,
+					2'b00,2'b00,2'b00,
+					2'b00,2'b00,2'b00};
+			end
+			if(!select) begin
+				// when button is pressed, the next frame will
+				// check if the move is valid
+				nx_state <= checkMove;
+			end
+		end
 		
-		//check valid location
-			if(isValid == 1'b0) begin 
-				//go back to play state?
-				nx_state <= play;
-			end
-			else begin
-				//check if win
-//				player = ~player;
-				nx_state <= checkWin;
-			end
-	 end
-	 checkWin: begin
-	 //checkWin
-	 
-	 //if not win, keep play
-	 //if win, go to respective win state
-		 if(winner == 2'b00) begin
-				nx_state <= play;
-		 end
-		 else if (winner == 2'b01) begin
-			nx_state <= p1Win;
-		 end
-		 else if (winner == 2'b01) begin
-			nx_state <= p2Win;
-		 end
-		 else if (winner == 2'b11) begin
-			nx_state <= tie;
-		 end 
-	 end
-	 p1Win: begin
-		 //player 1 wins
-		 
-		 //-> reset board
-		 nx_state <= reset;
-	 end
-	 p2Win: begin
-		 //player 2 wins
-		 
-		 //-> reset board
-		 nx_state <= reset;
-	 end
-	 tie: begin
-		 //tie
-		 
-		 //-> reset board
-		 nx_state <= reset;
-	 end
-	 reset: begin
-		 //reset board
-		 
-		 //back to play
-		 nx_state <= play;
-	 end
- endcase
+	
+	
+		// frame rate related logic
+		counter <= counter + 1;
+		if (counter >= clocks_per_frame) begin
+			counter <= 0;
+			
+			
+			// state logic
+			case(pr_state)
+				play: begin
+					//user input
+					//TODO
+					nx_state <= play;
+				end
+				checkMove: begin
+					//check valid location
+					if(is_valid(move, board)) begin 
+						if(player == 0) begin
+							board[nine_to_four(location)] <= '{2'b01};
+						end
+						else begin
+							board[nine_to_four(location)] <= '{2'b10};
+						end
+						nx_state <= checkWin;
+					end
+				end
+				checkWin: begin
+					//checkWin
+				 
+					//if not win, keep play
+					//if win, go to respective win state
+					case(check_win(board))
+						2'b00: begin
+							nx_state <= play;
+						end
+						2'b01: begin
+							nx_state <= p1Win;
+						end
+						2'b10: begin
+							nx_state <= p2Win;
+						end
+						2'b11: begin
+							nx_state <= tie;
+						end
+					endcase 
+				end
+				p1Win: begin
+					 //player 1 wins
+					 
+					 //-> reset board
+					nx_state <= reset;
+				end
+				p2Win: begin
+					 //player 2 wins
+					 
+					 //-> reset board
+					nx_state <= reset;
+				end
+				tie: begin
+					//tie
+			 
+					//-> reset board
+					nx_state <= reset;
+				end
+				reset: begin
+					//reset board
+			 
+					//back to play
+					nx_state <= play;
+				end
+			endcase	
+		end
+	end
 endmodule 
 
 // if ({x,y} == 2’b01) begin z=1’b1; nx_state <= B; end
