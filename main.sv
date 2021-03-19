f6f66901972726bd39c58cda95056488883b986e
AllenKaplan <allenkaplan07@gmail.com>
2021-03-19 14:44:53 -0400

    updated main

--- main.sv
+++ main.sv
@@ -1,12 +1,15 @@
 //Part 1: Module header:
 module main(
- input logic clk, rst, x, y,
+ input logic clk, rst, move
  output logic z);
  
 //Part 2: Declarations:
 //FSM-related declarations:
-typedef enum logic [1:0] {A, B, C, D}. state;
+typedef enum logic [2:0] {play, checkWin, p1Win, p2Win, tie, reset}. state;
 state pr_state, nx_state;
+
+reg [1:0] board [8:0];
+
 
 //Timer-related declarations:
 const logic [7:0] T1 = <value>, tmax = <value>;
@@ -19,22 +22,35 @@ always_ff @(posedge clk, posedge rst)
 
 //FSM combinational logic: always_comb
 case (pr_state)
- A: begin
- if ({x,y} == 2’b01) begin z=1’b1; nx_state <= B; end
- else begin z=1’b0; nx_state <= A; end
+ play: begin
+	//user input
+	//check valid location
+	//check if win
 end
- B: begin
- if (t>=T1) begin z<=1’b0; nx_state <= D; end
- else begin z=1’b1; nx_state <= B; end
+ checkWin: begin
+ //checkWin
+ //either go to win state
+ //else, go to play
  end
- C: begin
- if ({x,y} == 2’b11 ) begin z=1’b0; nx_state <= B end;
- else begin z <= 1’b1; nx_state <= C; end
+ p1Win: begin
+ //player 1 wins
+ //-> reset board
 end
- D: begin
- if (x == 1’b1) begin z=1’b0; nx_state <= A; end
- else begin z <= 1’b1; nx_state <= C end
+ p2Win: begin
+ //player 2 wins
+ //-> reset board
+end
+ tie: begin
+ //tie
+ //-> reset board
+ end
+ reset: begin
+ //reset board
+ //-> play
  end
 endcase
 
 endmodule 
+
+// if ({x,y} == 2’b01) begin z=1’b1; nx_state <= B; end
+// else begin z=1’b0; nx_state <= A; end
